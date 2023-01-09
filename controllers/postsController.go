33c27d14f077dea175eeebdb0adf08cc1109d2d6
Sunday  Jimoh <31360475+sunday4me@users.noreply.github.com>
2023-01-09 02:12:59 +0100

    controllers folder

---
+++ controllers/postsController.go
@@ -0,0 +1,102 @@
+package controllers
+
+import (
+	"go-crud/initializers"
+	"go-crud/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+func PostsCreate(c *gin.Context) {
+
+	// Get data off req body
+	var body struct {
+		Body  string
+		Title string
+	}
+
+	c.Bind(&body)
+
+	//Create a post
+
+	post := models.Post{Title: body.Title, Body: body.Body}
+
+	result := initializers.DB.Create(&post) // pass pointer of data to Create
+
+	if result.Error != nil {
+		c.Status(400)
+		return
+	}
+
+	// Return it
+	c.JSON(200, gin.H{
+		"post": "post",
+	})
+}
+
+func PostsIndex(c *gin.Context) {
+	// Get the posts
+	var posts []models.Post
+
+	initializers.DB.Find(&posts)
+
+	//Respond with them
+	c.JSON(200, gin.H{
+		"post": "post",
+	})
+
+}
+
+func PostsShow(c *gin.Context) {
+	// Get id off url
+	id := c.Param("id")
+
+	//Get the post
+	var post models.Post
+	initializers.DB.First(&posts, id)
+
+	//Respond with them
+	c.JSON(200, gin.H{
+		"post": "post",
+	})
+}
+
+func PostsUpdate(c *gin.Context) {
+	// get the id off the url
+	id := c.Param("id")
+
+	//Get the data off req body
+	var body struct {
+		Body  string
+		Title string
+	}
+
+	c.Bind(&body)
+
+	// Find the post were updating
+	var post models.Post
+	initializers.DB.First(&posts, id)
+
+	//update it
+	initializers.DB.Model(&post).Updates(models.Post{
+		Title: body.Title,
+		Body:  body.Body,
+	})
+
+	// Respond with it
+	c.JSON(200, gin.H{
+		"post": "post",
+	})
+
+}
+
+func PostsDelete(c *gin.Context) {
+	// get the id off the url
+	id := c.Param("id")
+
+	//Delete the Post
+	initializers.DB.Delete(model(&post){},id)
+
+	// Respond with it
+	c.Status(200)
+}
